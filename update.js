fc1dc43b0821fbb76f371c05f0d23887332fbd0e
Elsa <eleadrwhitmore@gmail.com>
2025-12-25 10:52:18 +0800

    modified homepage and added progress part

---
+++ update.js
@@ -0,0 +1,6 @@
+const siteStats = [
+  { label: "📚 课程笔记搬运", percent: 85 },
+  { label: "💡 LeetCode 模型", percent: 55 },
+  { label: "🐟 德语 B1 笔记", percent: 30 },
+  { label: "📄 论文库更新", percent: 20 }
+];
